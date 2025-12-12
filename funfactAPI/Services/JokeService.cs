57c2873d168e745e5f7cca37ce592ba5b40447ff
Abdallah <D00287084@student.dkit.ie>
2025-12-12 15:29:07 +0000

    implemented random joke service

--- funfactAPI/Services/JokeService.cs
+++ funfactAPI/Services/JokeService.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using funfactAPI.Modelas;
 
 namespace funfactAPI.Services
 {
-    internal class JokeService
+    // Implements IJokeService
+    public class JokeService : IJokeService
     {
+        public Joke GetRandomJoke()
+        {
+            using var client = new HttpClient();
+
+            // Call the joke API
+            string json = client
+                .GetStringAsync("https://official-joke-api.appspot.com/random_joke")
+                .Result;
+
+            // Parse the JSON response
+            using var doc = JsonDocument.Parse(json);
+
+            // Create and return a Joke object
+            return new Joke
+            {
+                Setup = doc.RootElement.GetProperty("setup").GetString(),
+                Punchline = doc.RootElement.GetProperty("punchline").GetString()
+            };
+        }
     }
 }
