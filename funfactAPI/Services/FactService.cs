e5c16d87c4d876cdda321762f7cc82bf1604a054
Abdallah <D00287084@student.dkit.ie>
2025-12-12 14:58:25 +0000

    implemented teh random fact service

--- funfactAPI/Services/FactService.cs
+++ funfactAPI/Services/FactService.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using funfactAPI.Modelas;
 
 namespace funfactAPI.Services
 {
-    internal class FactService
+
+    public class FactService : IFactService
     {
+        public Fact GetRandomFact()
+        {
+            // used HttpClient to make a HTTP requests
+            using var client = new HttpClient();
+
+            // Call the random fact API (GET request)
+            string json = client
+                .GetStringAsync("https://uselessfacts.jsph.pl/random.json?language=en")
+                .Result;
+
+            using var doc = JsonDocument.Parse(json);
+
+            // Extract the "text" field from the JSON
+            string text = doc.RootElement.GetProperty("text").GetString();
+
+            // Return a Fact object with the data
+            return new Fact
+            { 
+                Text = text
+            };
+        }
     }
 }
