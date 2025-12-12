6c163933221a15e51ba9306c42f052ec17b96497
Abdallah <D00287084@student.dkit.ie>
2025-12-12 15:44:29 +0000

    made main program

--- funfactAPI/Program.cs
+++ funfactAPI/Program.cs
@@ -1,12 +1,31 @@
-﻿namespace funfactAPI
-// testing commit
+﻿using funfactAPI.Services;
+using funfactAPI.Modelas;
 
+namespace funfactAPI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // create service objects using their interfaces
+            IFactService factService = new FactService();
+            IJokeService jokeService = new JokeService();
+
+            // services
+            Fact fact = factService.GetRandomFact();
+            Joke joke = jokeService.GetRandomJoke();
+
+            // display results
+            Console.WriteLine("=== Random  FACT ===\n");
+            Console.WriteLine(fact.Text + "\n");
+
+
+            Console.WriteLine("=== A JOKE ===");
+            Console.WriteLine(joke.Setup);
+            Console.WriteLine(joke.Punchline);
+
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
         }
     }
 }
